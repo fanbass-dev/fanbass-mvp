9b67cb1764aaa34e814764b6f062f96bbf33fcc0
fanbass-dev <admin@fanbass.io>
2025-05-31 19:06:10 +0000

    undo supabase url change

--- frontend/src/supabaseClient.ts
+++ frontend/src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://auth.fanbass.io'
+const supabaseUrl = 'https://vkvhrearjmmwiuvvmcib.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrdmhyZWFyam1td2l1dnZtY2liIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwNTE3NjMsImV4cCI6MjA2MzYyNzc2M30.yjGv1br6hEnuvkTUEH7S8PidI5dly9Q5rvfcdfehpYo'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
