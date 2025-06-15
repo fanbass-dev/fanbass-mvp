cc34c473567e60189634df70394046df5b56818f
Ariel Lasry <admin@fanbass.io>
2025-06-15 06:27:25 -0600

    add logging

--- frontend/src/supabaseClient.ts
+++ frontend/src/supabaseClient.ts
@@ -3,4 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY!
 
+console.log("REACT_APP_SUPABASE_URL:", process.env.REACT_APP_SUPABASE_URL);
+console.log("REACT_APP_SUPABASE_ANON_KEY:", process.env.REACT_APP_SUPABASE_ANON_KEY);
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
